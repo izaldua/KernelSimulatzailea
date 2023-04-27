4778afe5e28d9f1323fc63971d336c4a150456f2
Iker Zaldua <96075417+izaldua@users.noreply.github.com>
2023-04-27 12:43:33 +0200

    Add files via upload

---
+++ seso.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <pthread.h>
+
+
+#define TENP_KOP 2
+
+
+pthread_mutex_t mutex;
+pthread_cond_t cond1, cond2;
+
+
+int done = 0;
+int sartu = 0;
+
+
+void *erlojua()
+{
+   printf("# Erlojua: Ondo iritsi naiz erlojura.\n");
+   done = 0;
+   while (1)
+   {
+       pthread_mutex_lock(&mutex);
+       sartu = 1;
+       while (done < TENP_KOP)
+           pthread_cond_wait(&cond1, &mutex);
+       done = 0;
+       sartu = 0;
+       pthread_cond_broadcast(&cond2);
+       pthread_mutex_unlock(&mutex);
+   }
+}
+
+
+void *timer1()
+{
+   printf("# Timer1: Ondo iritsi naiz timer-era.\n");
+
+
+   int tick1 = 0;
+   pthread_mutex_lock(&mutex);
+   while (1)
+   {
+
+
+       done++;
+       if (tick1 == 200)
+       {
+           printf("\nIEPA\n");
+           tick1 = 0;
+       }
+       else
+       {
+           tick1++;
+           printf("->   tick1: %d\n", tick1);
+       }
+       pthread_cond_signal(&cond1);
+       pthread_cond_wait(&cond2, &mutex);
+   }
+}
+
+
+void *timer2()
+{
+   printf("# Timer2: Ondo iritsi naiz timer-era.\n");
+
+
+   int tick2 = 0;
+   pthread_mutex_lock(&mutex);
+   while (1)
+   {
+       done++;
+       if (tick2 == 100)
+       {
+           printf("\nIEPE\n");
+           tick2 = 0;
+       }
+       else
+       {
+           tick2++;
+           printf("->   tick2: %d\n", tick2);
+       }
+       pthread_cond_signal(&cond1);
+       pthread_cond_wait(&cond2, &mutex);
+   }
+}
+
+
+int main(int argc, char *argv[])
+{
+   printf("=========================\nSistema ondo hasieratu da\n=========================\n\n\n");
+
+
+   // MUTEX eta bere testuinguru osoa hemen hasieratzen da
+   pthread_mutex_init(&mutex, NULL);
+   printf("Mutexa ondo hasieratu da\n");
+   pthread_cond_init(&cond1, NULL);
+   printf("Lehen baldintza ondo hasieratu da\n");
+   pthread_cond_init(&cond2, NULL);
+   printf("Bigarren baldintza ondo hasieratu da\n");
+
+
+   // HARIEN testuinguru osoa hemen hasieratzen da
+   pthread_t erl, tim1, tim2;
+
+
+   if (pthread_create(&erl, NULL, &erlojua, NULL) != 0) // Hariak sortzeko momentuan existitzen badira dagoeneko, honek "errorea" bueltatuko du
+       perror("Failed to create threads");
+   if (pthread_create(&tim1, NULL, &timer1, NULL) != 0)
+       perror("Failed to create threads");
+   if (pthread_create(&tim2, NULL, &timer2, NULL) != 0)
+       perror("Failed to create threads");
+
+
+   printf("\n\n");
+
+
+   // HARIEN testuinguru osoa hemen joineatzen da dena ondo amaitzeko
+   if (pthread_join(erl, NULL) != 0)
+       perror("Failed to join threads");
+   else
+       printf("Erlojuaren haria ondo amaitu da\n");
+   if (pthread_join(tim1, NULL) != 0)
+       perror("Failed to join threads");
+   else
+       printf("Timer1-aren haria ondo amaitu da\n");
+   if (pthread_join(tim2, NULL) != 0)
+       perror("Failed to join threads");
+   else
+       printf("Timer2-aren haria ondo amaitu da\n");
+
+
+   printf("\n\n");
+
+
+   // MUTEX-aren testuinguru osoa hemen amaitzen da dena suntsituz
+   pthread_mutex_destroy(&mutex);
+   printf("Mutexa ondo amaitu da\n");
+   pthread_cond_destroy(&cond1);
+   printf("Lehen baldintza ondo amaitu da\n");
+   pthread_cond_destroy(&cond2);
+   printf("Bigarren baldintza ondo amaitu da\n");
+
+
+   return 0;
+}
