86f4f9f934a7b7c6f3cf88725e6cf818bba70379
Iker Zaldua <ikerzalduni@gmail.com>
2023-06-16 16:24:36 +0200

    Version 1.0 de la entrega de SE

--- KernelSimulatzailea/include/clock.h
+++
@@ -1,6 +0,0 @@
-#ifndef CLOCK_H
-#define CLOCK_H
-
-extern void *erlojua();
-
-#endif /* CLOCK_H */
--- KernelSimulatzailea/include/timer.h
+++
@@ -1,7 +0,0 @@
-#ifndef TIMER_H
-#define TIMER_H
-
-extern void *timer_scheduler();
-extern void *timer_loader();
-
- #endif /* TIMER_H */
